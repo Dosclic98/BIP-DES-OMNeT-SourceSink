2ff65b4fa306f75e77484b373bf80efe0cd25a8e
Davide Savarro <davidesavarro98@gmail.com>
2025-03-11 15:17:40 +0100

    Complete project

---
+++ src/source/Source.cc
@@ -0,0 +1,40 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#include "Source.h"
+
+Define_Module(Source);
+
+Source::Source() {
+    this->timerMessage = NULL;
+}
+
+Source::~Source() {
+    cancelAndDelete(timerMessage);
+}
+
+void Source::initialize() {
+    timerMessage = new cMessage("timer");
+    scheduleAt(simTime(), timerMessage);
+}
+
+void Source::handleMessage(cMessage *msg) {
+    ASSERT(msg == timerMessage);
+
+    cMessage* job = new cMessage("job");
+    send(job, "out");
+
+    scheduleAt(simTime()+par("sendInterval").doubleValue(), timerMessage);
+}
